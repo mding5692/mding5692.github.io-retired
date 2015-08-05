75797e15c33c0c145fe9dcc15d3b3ac1e1ccf843
Michael <m.ding5692@gmail.com>
2015-08-04 21:14:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $(document).ready( function() {
-  alert("Still making my new website and trashed my old one, if my naked and obscene website has offended your eyes, please feel free to not sue me, thanks!");
+  alert("Still making my new website and trashed my old one, if my naked and obscene website has offended your eyes, please feel free to avert your delicate, pretty eyes. Thanks!");
 });
