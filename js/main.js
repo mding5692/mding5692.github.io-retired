aa6f3b79feb420d10f98091593226a7b6ad23a11
Michael <m.ding5692@gmail.com>
2015-08-04 21:13:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-$(document).ready({
-  alert("Still making my new website and trashed my old one, if my obscene website has offended your eyes, please feel free to not sue me, thanks!");
-
+$(document).ready( function() {
+  alert("Still making my new website and trashed my old one, if my naked and obscene website has offended your eyes, please feel free to not sue me, thanks!");
 });
